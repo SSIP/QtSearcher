66279a75ee98b2127dc8a07408e877f26ca6e69a
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-11-16 13:21:41 +0100

    Change default config

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -32,6 +32,7 @@ void  setStdConf(config* cfg) {
 	cfg->devCrit = 1.5;
 	cfg->checkSNR = 5.0;
 	cfg->checkRadius = 1.0;
-	cfg->verbosity = 2;
+	cfg->verbosity = 3;
 	cfg->rayBrightnessThreshold = 0.3;
+	cfg->maxDiameter = 0.4;
 }
