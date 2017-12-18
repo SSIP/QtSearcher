aa64cada6ba477e79b2d43e9a43c6806523e03b6
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-12-18 18:29:47 +0100

    Update function comments

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,9 +1,9 @@
 #include "helpers.h"
 /* Convert 8 bit integer to QImage
  *
- * Param *sourceImage is the image that will be converted
- * Param *cfg is the global configuration containing the image resolution
- * Param *curQimg is the reference to the target of the conversion
+ @sourceImage:    image that will be converted
+ @cfg:            global configuration containing the image resolution
+ @curQimg:        reference to the target of the conversion
  */
 void toQimage8Bit(uint8_t *sourceImage, config *cfg, QImage *curQimg)
 {
@@ -21,9 +21,9 @@ void toQimage8Bit(uint8_t *sourceImage, config *cfg, QImage *curQimg)
  * The diff image contains values arouond 0. To convert it to 8 bit unsigned int, we add
  * 128 and expect that no values are too large.
  *
- * Param *sourceImage is the image that will be coneverted
- * Param *cfg is the global configuration containing the image resolution
- * Param *curQimg is the reference to the target of the conversion
+ * @sourceImage:    image that will be coneverted
+ * @cfg:            global configuration containing the image resolution
+ * @curQimg:        reference to the target of the conversion
  */
 void toQimage16Bit(int16_t *sourceImage, config *cfg, QImage *curQimg)
 {
@@ -41,7 +41,7 @@ void toQimage16Bit(int16_t *sourceImage, config *cfg, QImage *curQimg)
 
 /* Set a standard configuration
  *
- * Param *cfg is the reference to the configuration struct
+ @cfg:            the reference to the configuration struct
  */
 void  modifyConfig(config* cfg) {
 	cfg->srcPath = QDir::homePath().toStdString();
